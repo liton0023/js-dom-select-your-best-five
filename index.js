fa8fc2fe23ddab841acca0477a98f05acdb2f710
Md Liton Mahmud <litonmahmud0023@gmail.com>
2023-02-03 07:32:13 +0600

    add-js

---
+++ index.js
@@ -0,0 +1,54 @@
+document.getElementById('select-player').addEventListener('click',function(){
+    // console.log('clicked')
+    const player1=document.getElementById('player-name-1');
+    const playerList1=player1.innerText;
+
+    const selectedPlayer=document.getElementById('player-list');
+    selectedPlayer.innerText=playerList1;
+
+    console.log(selectedPlayer);
+
+})
+
+document.getElementById('calculate-btn').addEventListener('click',function(){
+const selectPlayer=document.getElementById('player-list');
+const playerList=selectPlayer.innerText;
+const playerNumber=parseFloat(playerList);
+
+
+const inputFild=document.getElementById('per-player-input');
+const inputvalue=inputFild.value;
+// console.log(inputvalue)
+
+const playerCost=playerNumber*inputvalue;
+// console.log(playerCost)
+    
+const playerExpence=document.getElementById('player-expence');
+playerExpence.innerText=playerCost;
+inputFild.value='';
+
+
+})
+
+document.getElementById('total-calculate-btn').addEventListener('click',function(){
+    const playerExpence=document.getElementById('player-expence');
+    const previousPlayerExpence=playerExpence.innerText;
+    const previousPlayerExpenceValue=parseFloat(previousPlayerExpence);
+
+    const mangerInput=document.getElementById('manger-input');
+    const mangerInputValue=mangerInput.value;
+    const mangerInputPrice=parseFloat(mangerInputValue);
+
+    const coachInput=document.getElementById('coach-input');
+    const coachInputValue=coachInput.value;
+    const coachInputPrice=parseFloat(coachInputValue);
+
+    const totalAmount=previousPlayerExpenceValue + mangerInputPrice + coachInputPrice;
+
+    const total=document.getElementById('total-cost');
+    total.innerText=totalAmount;
+
+    mangerInput.value='';
+    coachInput.value='';
+})
+
